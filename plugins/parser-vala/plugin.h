d6730de5c0c95ae377299f538418f9086e575947
Moritz Lüdecke <ritze@skweez.net>
2012-06-03 11:50:11 +0200

    create parser-vala
    copy parser-cxx to parser-vala and adapt the files to vala

---
+++ plugins/parser-vala/plugin.h
@@ -0,0 +1,58 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+    plugin.h
+    Copyright (C) 2000 Naba Kumar
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef _PLUGIN_H_
+#define _PLUGIN_H_
+
+#include <libanjuta/anjuta-plugin.h>
+
+extern GType parser_vala_plugin_get_type (GTypeModule *module);
+#define ANJUTA_TYPE_PLUGIN_PARSER_VALA         (parser_vala_plugin_get_type (NULL))
+#define ANJUTA_PLUGIN_PARSER_VALA(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), ANJUTA_TYPE_PLUGIN_PARSER_VALA, ParserValaPlugin))
+#define ANJUTA_PLUGIN_PARSER_VALA_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST ((k), ANJUTA_TYPE_PLUGIN_PARSER_VALA, ParserValaPluginClass))
+#define ANJUTA_IS_PLUGIN_PARSER_VALA(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), ANJUTA_TYPE_PLUGIN_PARSER_VALA))
+#define ANJUTA_IS_PLUGIN_PARSER_VALA_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), ANJUTA_TYPE_PLUGIN_PARSER_VALA))
+#define ANJUTA_PLUGIN_PARSER_VALA_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), ANJUTA_TYPE_PLUGIN_PARSER_VALA, ParserValaPluginClass))
+
+typedef struct _ParserValaPlugin ParserValaPlugin;
+typedef struct _ParserValaPluginClass ParserValaPluginClass;
+
+struct _ParserValaPlugin {
+	AnjutaPlugin parent;
+	
+	GSettings* settings;
+	GSettings* editor_settings;
+	gint editor_watch_id;
+	gboolean support_installed;
+	GObject *current_editor;
+	const gchar *current_language;
+
+	/* Assist */
+	CppJavaAssist *assist;
+	
+	/* Preferences */
+	GtkBuilder* bxml;
+};
+
+struct _ParserValaPluginClass {
+	AnjutaPluginClass parent_class;
+};
+
+#endif
