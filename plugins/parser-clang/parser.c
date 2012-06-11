875e09bd3b750043b42ceabe1708e6d245151547
Moritz Lüdecke <ritze@skweez.net>
2012-06-11 10:34:04 +0200

    add experimental code to get definition of marked code

--- plugins/parser-clang/parser.c
+++ plugins/parser-clang/parser.c
@@ -23,21 +23,126 @@
 #include "parser.h"
 
 //struct _ParserClangParserPriv {
+	const gchar* path;
 	CXTranslationUnit translation_unit;
+	CXFile file;
 //}
+
+static GList*
+parser_clang_parser_get_diagnostics ()
+{
+	GList *list = NULL;
+	
+	guint numDiagnostics = clang_getNumDiagnostics (translation_unit);
+	guint i;
+	
+	for (i = 0; i != numDiagnostics; ++i)
+		list = g_list_append (list, clang_getDiagnostic (translation_unit, i));
+	
+	return list;
+}
+
+static void
+parser_clang_parser_diag (CXDiagnostic *diag)
+{
+	CXString string = clang_formatDiagnostic (diag,clang_defaultDiagnosticDisplayOptions());
+	g_warning ("%s", clang_getCString (string));
+	clang_disposeString (string);
+}
+
+static void
+parser_clang_parser_parse (const gchar *unsavedContent)
+{
+	//some test code
+	if (unsavedContent) {
+		g_warning ("reparse...");
+		
+		struct CXUnsavedFile *unsaved = NULL;	
+		unsaved = g_malloc (2 * sizeof (struct CXUnsavedFile));
+		unsaved[0].Filename = path;
+		unsaved[0].Contents = unsavedContent;
+		//TODO: works this really?
+		unsaved[0].Length = g_strv_length ((gchar**) unsavedContent);
+		unsaved[1].Filename = NULL;
+		unsaved[1].Contents = NULL;
+		unsaved[1].Length = 0;
+
+		gint error = clang_reparseTranslationUnit (translation_unit, 1,
+						unsaved, clang_defaultReparseOptions (translation_unit));
+
+		//TODO: act accordingly...
+		if (error)
+		{
+			g_warning ("Could not reparse! abort here...");
+			return;
+		}
+
+		g_free (unsaved);
+	}
+	
+	GList *diags = parser_clang_parser_get_diagnostics ();
+
+	g_list_foreach (diags, (GFunc) parser_clang_parser_diag, NULL);
+}
+
+static void
+parser_clang_parser_get_calltip (guint line, guint column)
+{
+	CXSourceLocation location = clang_getLocation (translation_unit, file, line, column);
+	CXCursor cursor = clang_getCursor (translation_unit, location);
+
+	//more test code:
+	CXString cursorSpelling = clang_getCursorDisplayName (cursor);
+	g_warning ("Cursor: %s", clang_getCString (cursorSpelling));
+	clang_disposeString (cursorSpelling);
+
+	//TODO: Why is definition always null?
+	CXCursor definition = clang_getCursorDefinition (cursor);
+
+	if (clang_Cursor_isNull (definition))
+	{
+		g_warning ("Cursor to the definition is null!");
+		return;
+	}
+
+	CXFile definitionFile;
+	guint definitionLine;
+	guint definitionColumn;
+	guint definitionOffset;
+	
+	CXSourceLocation definitionLocation = clang_getCursorLocation (definition);
+	clang_getInstantiationLocation (definitionLocation, &definitionFile, &definitionLine, &definitionColumn, &definitionOffset);
+	
+	CXString definitionFileName = clang_getFileName(definitionFile);
+	g_warning ("Definition: Cursor location: %s, %d, %d, %d",
+		clang_getCString(definitionFileName), definitionLine, definitionColumn, definitionOffset);
+	
+	//return definitionOffset;
+}
 
 void
 parser_clang_parser_init (const gchar* full_file_path)
 {
 	g_warning ("initiate new translation unit instance for %s", full_file_path);
-	
+
+	path = full_file_path;
 	CXIndex index = clang_createIndex (0, 0);
-	//TODO: set num_unsaved_files and unsaved_files...
-	unsigned num_unsaved_files = 0;
-	struct CXUnsavedFile * unsaved_files = NULL;
 	
-	translation_unit = clang_createTranslationUnitFromSourceFile (index, full_file_path, 0, NULL, num_unsaved_files, unsaved_files);
+	translation_unit = clang_createTranslationUnitFromSourceFile (index, path, 0, NULL, 0, NULL);
+	file = clang_getFile(translation_unit, path);
+	
 	clang_disposeIndex (index);
+
+	if (!translation_unit)
+	{
+		g_warning ("Could not parse file.");
+		return;
+	}
+	
+	//even more test code
+	parser_clang_parser_parse (NULL);
+	parser_clang_parser_get_calltip (129, 66);
+	parser_clang_parser_get_calltip (40, 3);
 }
 
 void
@@ -49,6 +154,9 @@ parser_clang_parser_deinit ()
 		clang_disposeTranslationUnit (translation_unit);
 		translation_unit = NULL;
 	}
+	
+	file = NULL;
+	path = NULL;
 }
 
 IAnjutaIterable *
@@ -59,19 +167,4 @@ parser_clang_parser_process_expression (const gchar *stmt,
 {
 	g_warning ("process expression...");
 	return NULL;
-}
-
-static void
-parser_clang_parser_parse ()
-{
-	//some test code
-	unsigned n;
-	unsigned i;
-	for (i = 0, n = clang_getNumDiagnostics (translation_unit); i != n; ++i) {
-		CXDiagnostic diag = clang_getDiagnostic (translation_unit, i);
-		CXString string = clang_formatDiagnostic (diag,
-								clang_defaultDiagnosticDisplayOptions());
-		g_warning ("%s", clang_getCString (string));
-		clang_disposeString (string);
-	}
 }
